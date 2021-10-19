93fa3a031c47edce37c0fe37ec90895d80d73704
Max Avramenko <71176948+darkforce1988@users.noreply.github.com>
2021-10-19 22:51:17 +0300

    solve 3.4-3.7, 3.9 tasks

--- 3.4-3.7, 3.9.py
+++ 3.4-3.7, 3.9.py
@@ -1,27 +1,42 @@
-guests = ['pasiypa' , 'санька' , 'алик']
-print(f"Привет, приходи на ужин, {guests[0].title()}.")
-print(f"Привет, приходи на ужин, {guests[1].title()}.")
-print(f"Привет, приходи на ужин, {guests[2].title()}.")
-print(f"Однако, {guests[0].title()} не сможет прийти из-за диареи.")
-guests[0] = 'костик'
-print(f"Привет, приходи на ужин, {guests[0].title()}.")
-print(f"Привет, приходи на ужин, {guests[1].title()}.")
-print(f"Привет, приходи на ужин, {guests[2].title()}.")
-guests.insert(0, "юрик")
-guests.insert(1, "собака")
-guests.append("Люба")
-print(f"Привет, приходи на ужин, {guests[0].title()}.")
-print(f"Привет, приходи на ужин, {guests[1].title()}.")
-print(f"Привет, приходи на ужин, {guests[2].title()}.")
-print(f"Привет, приходи на ужин, {guests[3].title()}.")
-print(f"Привет, приходи на ужин, {guests[4].title()}.")
-print(f"Привет, приходи на ужин, {guests[5].title()}.")
+guests = ['paSiYupa', 'санька', 'алик']
+for i in range(0, len(guests)):
+    guests[i] = guests[i].title()
+
+print(f"Привет, приходи на ужин, {guests[0]}.")
+print(f"Привет, приходи на ужин, {guests[1]}.")
+print(f"Привет, приходи на ужин, {guests[2]}.")
+
+print(f"Однако, {guests[0]} не сможет прийти из-за диареи.")
+
+guests[0] = 'костик'.title()
+print(f"Привет, приходи на ужин, {guests[0]}.")
+print(f"Привет, приходи на ужин, {guests[1]}.")
+print(f"Привет, приходи на ужин, {guests[2]}.")
+
+guests.insert(0, "юрик".title())
+guests.insert(1, "собака".title())
+guests.append("люба".title())
+print(f"Привет, приходи на ужин, {guests[0]}.")
+print(f"Привет, приходи на ужин, {guests[1]}.")
+print(f"Привет, приходи на ужин, {guests[2]}.")
+print(f"Привет, приходи на ужин, {guests[3]}.")
+print(f"Привет, приходи на ужин, {guests[4]}.")
+print(f"Привет, приходи на ужин, {guests[5]}.")
+
 print("Упс, мы можем пригласить только двух гостей.")
 while len(guests) > 2:
     minus = guests.pop(0)
     print(f"Извини, {minus}, я отменяю своё приглашение.")
-print(f"Предложение насчёт ужина в силе, {guests[0].title()}.")
-print(f"Предложение насчёт ужина в силе, {guests[1].title()}.")
-del guests[0]
-del guests[0]
+
+print(f"Предложение насчёт ужина в силе, {guests[0]}.")
+print(f"Предложение насчёт ужина в силе, {guests[1]}.")
+
+print("Количество приглашённых людей на ужин - " + str(len(guests)))
+print(sorted(guests))
+
+guests.reverse()
+print(guests)
+
+while len(guests) > 0:
+    del guests[0]
 print(guests)
