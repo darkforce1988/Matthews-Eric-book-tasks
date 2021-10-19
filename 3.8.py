9c703c21d0e4bb1f41a3095404f57c5d55d09134
Max Avramenko <71176948+darkforce1988@users.noreply.github.com>
2021-10-19 23:16:37 +0300

    solve 3.8 task

---
+++ 3.8.py
@@ -0,0 +1,25 @@
+countries = ['Spain', 'France', 'UK', 'Norway', 'Iceland']
+
+print(countries)
+
+print(sorted(countries))
+
+print(countries)
+
+print(sorted(countries, reverse=True))
+
+print(countries)
+
+countries.reverse()
+print(countries)
+
+countries.reverse()
+print(countries)
+
+countries.sort()
+print(countries)
+
+countries.sort(reverse=True)
+print(countries)
+
+# print(countries[5])
