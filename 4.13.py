0952cd480c6f5f75e58d7bfd103a6fd8c47f4edb
Max Avramenko <71176948+darkforce1988@users.noreply.github.com>
2021-10-19 17:49:00 +0300

    solve 4.13 task

---
+++ 4.13.py
@@ -0,0 +1,10 @@
+meals = ('draniki', 'kartoshka', 'pizza', 'WHISKEY')
+for meal in meals:
+    print(meal)
+
+# we cant do that:
+# meals[0] = 'olivki'
+
+meals = ('new meal1', 'new meal2', 'new meal3')
+for new_meals in meals:
+    print(new_meals)
