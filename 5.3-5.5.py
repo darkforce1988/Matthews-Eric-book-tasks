2fd5391502d4c080d1f46cc951f4a06de2e0073d
Max Avramenko <71176948+darkforce1988@users.noreply.github.com>
2021-10-26 01:08:00 +0300

    solve 5.3-5.5 tasks

---
+++ 5.3-5.5.py
@@ -0,0 +1,36 @@
+alien_color = 'green'
+if 'green' in alien_color:
+    print('You got 5 points')
+
+if 'red' in alien_color:
+    print('You got 10 points')
+
+alien_color = 'red'
+if 'green' in alien_color:
+    print('You got 5 points')
+else:
+    print('You got 10 points')
+
+alien_color = 'red'
+if 'green' in alien_color:
+    print('You got 5 points')
+elif 'red' in alien_color:
+    print('You got 10 points')
+else:
+    print('You got 15 points')
+
+alien_color = 'green'
+if 'green' in alien_color:
+    print('You got 5 points')
+elif 'red' in alien_color:
+    print('You got 10 points')
+else:
+    print('You got 15 points')
+
+alien_color = 'yellow'
+if 'green' in alien_color:
+    print('You got 5 points')
+elif 'red' in alien_color:
+    print('You got 10 points')
+else:
+    print('You got 15 points')
