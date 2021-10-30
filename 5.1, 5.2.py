2a20810a23a693f2e68c090564dd6fe88269d2b1
Max Avramenko <71176948+darkforce1988@users.noreply.github.com>
2021-10-30 16:13:24 +0300

    solve 5.1, 5.2 tasks

---
+++ 5.1, 5.2.py
@@ -0,0 +1,45 @@
+>>> a = 10
+>>> a < 5
+False
+>>> a > 4
+True
+>>> a == 4
+False
+>>> a == 10
+True
+>>> b = 5
+>>> b > 4
+True
+>>> b < 2
+False
+>>> b != 44
+True
+>>> b == 5
+True
+>>> b < 999
+True
+>>> b == 1
+False
+
+>>> name = 'Axe'
+>>> name.lower() == 'axe'
+True
+>>> name.lower() == 'Axe'
+False
+>>> listok = ['a', 'b', 'c']
+>>> 'a' in listok
+True
+>>> 'd' in listok
+False
+>>> 'a' not in listok
+False
+>>> 'EZ' not in listok
+True
+>>> a == 10 or b == 4
+True
+>>> a == 10 and b == 5
+True
+>>> a < 20 and b > 5
+False
+>>> a < 20 and b > 2
+True
