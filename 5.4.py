471d2e92775f08b192cc7528b73d8a77c6ac6dc9
Max Avramenko <71176948+darkforce1988@users.noreply.github.com>
2021-10-30 16:53:33 +0300

    solve 5.4 task

---
+++ 5.4.py
@@ -0,0 +1,13 @@
+alien_color = 'green'
+if alien_color == 'green':
+    points = 5
+else:
+    points = 10
+print(f'You got {points} points.')
+
+alien_color = 'red'
+if alien_color == 'green':
+    points = 5
+else:
+    points = 10
+print(f'You got {points} points.')
