543b2ed051f40ecdbdc0edbef2ec5c10e191d639
Max Avramenko <71176948+darkforce1988@users.noreply.github.com>
2021-10-30 17:02:25 +0300

    solve 5.7 task

---
+++ 5.7.py
@@ -0,0 +1,16 @@
+favorite_fruits = ['Apples', 'Oranges', 'Pears']
+fruit = 'Strawberry'
+if fruit in favorite_fruits:
+    print(f"You really like {fruit}")
+fruit = 'Apples'
+if fruit in favorite_fruits:
+    print(f"You really like {fruit}")
+fruit = 'Oranges'
+if fruit in favorite_fruits:
+    print(f"You really like {fruit}")
+fruit = 'Lemons'
+if fruit in favorite_fruits:
+    print(f"You really like {fruit}")
+fruit = 'Pears'
+if fruit in favorite_fruits:
+    print(f"You really like {fruit}")
