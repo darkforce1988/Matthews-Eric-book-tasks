10e422b90759c657ef28a7c23e8117c3e4384429
Max Avramenko <71176948+darkforce1988@users.noreply.github.com>
2021-10-30 17:03:36 +0300

    solve 5.6 task

---
+++ 5.6.py
@@ -0,0 +1,13 @@
+age = 33
+if age < 2:
+    print('Baby')
+elif age < 4:
+    print('Kid')
+elif age < 13:
+    print('Child')
+elif age < 20:
+    print('Teen')
+elif age < 65:
+    print('Adult')
+else:
+    print('Old man')
