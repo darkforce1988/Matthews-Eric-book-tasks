fe661f676a886ddfd803d537663eecef0bbc66b0
Max Avramenko <71176948+darkforce1988@users.noreply.github.com>
2021-11-03 00:21:00 +0300

    solve 6.2 task

---
+++ Chapter 06/6.2.py
@@ -0,0 +1,7 @@
+fav_numbers = {'Alex': '3', 'Konstantine': '9', 'Alik': '21', 'Dennis': '33',
+'Max': '100'}
+print(f"Alex favorite number is {fav_numbers['Alex']}")
+print(f"Konstantine favorite number is {fav_numbers['Konstantine']}")
+print(f"Alik favorite number is {fav_numbers['Alik']}")
+print(f"Dennis favorite number is {fav_numbers['Dennis']}")
+print(f"Max favorite number is {fav_numbers['Max']}")
