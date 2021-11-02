ddb8843f288b742466a0249354981d4da8a1b0a5
Max Avramenko <71176948+darkforce1988@users.noreply.github.com>
2021-11-03 00:21:16 +0300

    solve 6.3 task

---
+++ Chapter 06/6.3.py
@@ -0,0 +1,11 @@
+glossary = {
+    'list': 'Lists are one of 4 built-in data types in Python '
+        'used to store collections of data.', 
+    'comment': 'Comments used to make the code more readable.', 
+    'string': 'Strings are arrays of bytes representing unicode characters.', 
+    'dictionary': 'Dictionaries are used to store data values in key:value pairs.', 
+    'method': 'A methods in python are somewhat similar to a functions, '
+        'except it is associated with object/classes.',
+    }
+print(glossary['list'], glossary['comment'], glossary['dictionary'], 
+    glossary['string'], glossary['method'], sep='\n')
