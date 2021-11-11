1d2e750cf3f62963798ca23f0cb15e0f82d1f54f
Max Avramenko <71176948+darkforce1988@users.noreply.github.com>
2021-11-11 20:35:07 +0300

    solve 6.9 task

---
+++ Chapter 06/6.9.py
@@ -0,0 +1,8 @@
+favorite_places = {
+    'Jan': ['Drichin', 'Pharmacy number 112'],
+    'Max': ['Lodnon', 'Brussels']
+}
+for name, places in favorite_places.items():
+    print(f"{name}'s favorite places are: ")
+    for place in places:
+        print(place)
