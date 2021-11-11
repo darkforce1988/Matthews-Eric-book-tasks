0df6c224baffa1697e1c2f527c128785c799b410
Max Avramenko <71176948+darkforce1988@users.noreply.github.com>
2021-11-11 21:35:41 +0300

    fix 6.7 task

--- Chapter 06/6.7.py
+++ Chapter 06/6.7.py
@@ -1,23 +1,23 @@
 Jan = {
-    'first_name': 'Jan', 
-    'last_name': 'Patsiupa', 
-    'age': '45', 
+    'first_name': 'Jan',
+    'last_name': 'Patsiupa',
+    'age': '45',
     'city': 'Drichin',
-    }
+}
 
 Max = {
     'first_name': 'Maxim',
     'last_name': 'Auramenka',
     'age': '33',
     'city': 'Minsk',
-    }
+}
 
 Alex = {
     'first_name': 'Alexander',
     'last_name': 'Vedro',
     'age': '27',
     'city': 'Minsk',
-    }
+}
 
 people = [Jan, Max, Alex]
 for name in people:
