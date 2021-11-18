7b99b805ee82d95da1e07905fb5ccb1ef68add7f
Max Avramenko <71176948+darkforce1988@users.noreply.github.com>
2021-11-19 01:55:35 +0300

    fix linter warnings in 6.1 task

--- Chapter 06/6.1.py
+++ Chapter 06/6.1.py
@@ -1,5 +1,9 @@
-Jan = {'first_name': 'Jan', 'last_name': 'Patsiupa', 'age': '45', 'city': 
-'Drichin'}
+Jan = {
+    'first_name': 'Jan',
+    'last_name': 'Patsiupa',
+    'age': '45',
+    'city': 'Drichin',
+}
 print(Jan['first_name'])
 print(Jan['last_name'])
 print(Jan['age'])
