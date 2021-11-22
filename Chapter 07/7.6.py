7872bac04e3b0b476659408e036bb0d47e80b8a6
Max Avramenko <71176948+darkforce1988@users.noreply.github.com>
2021-11-22 03:22:45 +0300

    solve 7.6 task

---
+++ Chapter 07/7.6.py
@@ -0,0 +1,13 @@
+active = True
+while active:
+    age = input("Enter your age." + '\n' + "Type 'quit' to exit: ")
+    if str(age) == 'quit':
+        break
+    if int(age) < 3:
+        ticket_price = 'free'
+    elif int(age) < 13:
+        ticket_price = "10$"
+    elif int(age) >= 13:
+        ticket_price = "15$"
+    print(f"A price of your ticket is: {ticket_price}.")
+    active = False
