98df433d051282e1636225bde35c65341b9b9cdd
Max Avramenko <71176948+darkforce1988@users.noreply.github.com>
2021-11-24 01:58:08 +0300

    fix 7.4 task

--- Chapter 07/7.4.py
+++ Chapter 07/7.4.py
@@ -1,12 +1,13 @@
 message = "When you are ready to make your order - type 'quit'."
-message += " Please enter a topping for your pizza: "
+message += "\nPlease enter a topping for your pizza: "
 topping = ""
 toppings = []
 while topping != 'quit':
     topping = input(message)
     if topping == 'quit':
         break
+    print(f"We will add a {topping} to your pizza!")
     toppings.append(topping)
 
 print(f"We'll make your pizza ready in a short time.\nSelected "
-      f"'toppings: {toppings}.")
+      f"toppings: {toppings}.")
