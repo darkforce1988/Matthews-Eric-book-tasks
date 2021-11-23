5aae9723fe41069d56d12c3d32539c933591ce3f
Max Avramenko <71176948+darkforce1988@users.noreply.github.com>
2021-11-23 23:47:02 +0300

    fix with 2 other solve options

--- Chapter 06/6.10.py
+++ Chapter 06/6.10.py
@@ -1,18 +1,24 @@
 fav_numbers = {
-    'Alex': ['3', '33333'],
-    'Konstantine': ['9'],
-    'Alik': ['21'],
+    'Alex': [3, 33333],
+    'Konstantine': [9],
+    'Alik': [21],
     'Dennis': [],
-    'Max': ['100', '50']
+    'Max': [100, 50]
 }
 
 for name, numbers in fav_numbers.items():
     if len(numbers) == 1:
-        ending = 'favorite number is:'
-        print(f"{name}'s {ending} {numbers}")
+        for number in numbers:
+            print(f"{name}'s favorite number is: {number}.")
     elif len(numbers) == 0:
-        ending = 'have no favorite numbers.'
-        print(f"{name} {ending}")
+        print(f"{name} have no favorite numbers.")
     else:
-        ending = 'favorite numbers are:'
-        print(f"{name}'s {ending} {numbers}")
+        list_of_numbers = str(numbers[0])
+        for number in numbers[1:]:
+            list_of_numbers += ', ' + str(number)
+        print(f"{name}'s favorite numbers are: {list_of_numbers}.")
+
+# else:
+#   print(f"{name}'s favorite numbers are: {', '.join(map(str, numbers))}.")
+#
+# print(*numbers, sep=', ')
