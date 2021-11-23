95656d64b1860496886c168a33b7674369712c82
Max Avramenko <71176948+darkforce1988@users.noreply.github.com>
2021-11-24 00:24:10 +0300

    update 6.11 by 6.12 task

--- Chapter 06/6.11.py
+++ Chapter 06/6.11.py
@@ -4,13 +4,13 @@ cities = {
         'population': '9,425,622',
         'fact': 'It is a very common mistake, but Big Ben is actually not the '
             'name for the iconic Tower in London. Big Ben is actually the name'
-            'for the clock in the Tower.'
+            ' for the clock in the Tower.'
         },
 
     'Paris': {
         'country': 'France',
         'population': '11,078,546',
-        'fact': 'around 5 million people per day use Paris Metro. After Moscow,'
+        'fact': 'Around 5 million people per day use Paris Metro. After Moscow,'
             ' it’s the busiest underground network in Europe.'
         },
 
@@ -22,6 +22,10 @@ cities = {
 }
 
 for city, infos in cities.items():
+    country = infos['country']
+    population = infos['population']
+    fact = infos['fact']
     print(f"{city} info:")
-    for info, value in infos.items():
-        print(f"\t{info}: {value}")
+    print(f"\tThis city is situated in {country}.")
+    print(f"\tPopulation of this city is {population}.")
+    print(f"\tInteresting fact about {city}: {fact}.")
