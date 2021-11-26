007386bdcba525f433eaa3cc320ba24c6edf40e7
Max Avramenko <71176948+darkforce1988@users.noreply.github.com>
2021-11-26 14:17:05 +0300

    fix message in input() function

--- Chapter 07/7.10.py
+++ Chapter 07/7.10.py
@@ -5,7 +5,8 @@ while asking:
     name = input("Please enter your name: ")
     response = input("Describe a vacation you dream of: ")
     vacation_responses[name] = response
-    response_end = input("Do you want to continue a survey? (yes/no) ")
+    response_end = input("Thanks for your answer.\n"
+                         "Continue with another interviewee? (yes/no) ")
 
     if response_end == 'no':
         asking = False
