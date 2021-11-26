210eef096ecfa88ef98ef7875b7e09bf94b14b83
Max Avramenko <71176948+darkforce1988@users.noreply.github.com>
2021-11-26 14:13:40 +0300

    fix 7.5 task

--- Chapter 07/7.5.py
+++ Chapter 07/7.5.py
@@ -1,8 +1,11 @@
-age = int(input("Enter your age: "))
-if age < 3:
-    ticket_price = "free"
-elif age < 13:
-    ticket_price = "10$"
-else:
-    ticket_price = "15$"
-print(f"A price of your ticket is: {ticket_price}.")
+active = True
+
+while active:
+    age = int(input("Enter your age: "))
+    if age < 3:
+        ticket_price = "free"
+    elif 13 > age >= 3:
+        ticket_price = "$10"
+    elif age >= 13:
+        ticket_price = "$15"
+    print(f"A price of your ticket is: {ticket_price}.")
