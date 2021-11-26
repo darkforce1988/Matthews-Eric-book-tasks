6f547fbd1543546033856e30083ab522de88fff0
Max Avramenko <71176948+darkforce1988@users.noreply.github.com>
2021-11-26 14:13:49 +0300

    fix 7.6 task

--- Chapter 07/7.6.py
+++ Chapter 07/7.6.py
@@ -1,13 +1,15 @@
 active = True
+
 while active:
     age = input("Enter your age." + "\n" + "Type 'quit' to exit: ")
-    if str(age) == 'quit':
+    if age == 'quit':
         break
-    if int(age) < 3:
+    age = int(age)
+    if age < 3:
         ticket_price = "free"
-    elif int(age) < 13:
-        ticket_price = "10$"
-    elif int(age) >= 13:
-        ticket_price = "15$"
+    elif 13 > age >= 3:
+        ticket_price = "$10"
+    elif age >= 13:
+        ticket_price = "$15"
     print(f"A price of your ticket is: {ticket_price}.")
     active = False
