728e52dc333c539646b59510eba92c81aca72b45
Max Avramenko <71176948+darkforce1988@users.noreply.github.com>
2021-11-26 14:22:25 +0300

    add 'pet_name' key into dictionary

--- Chapter 06/6.8.py
+++ Chapter 06/6.8.py
@@ -1,7 +1,7 @@
-Kosha = {'pet_type': 'cat', 'pet_owner': 'Max Avramenko'}
-Zver = {'pet_type': 'dog', 'pet_owner': 'Dmitry K.'}
+Kosha = {'pet_name': 'Kosha', 'pet_type': 'cat', 'pet_owner': 'Max Avramenko'}
+Zver = {'pet_name': 'Zver', 'pet_type': 'dog', 'pet_owner': 'Dmitry K.'}
 
 pets = [Kosha, Zver]
 
 for pet in pets:
-    print(f"{pet['pet_owner']}'s pet is a {pet['pet_type']}.")
+    print(f"{pet['pet_name']} is a {pet['pet_type']}. {pet['pet_owner']} is it's owner.")
