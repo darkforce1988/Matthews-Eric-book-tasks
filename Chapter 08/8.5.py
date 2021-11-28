39f01a60839be97a37d032743bbb3d5b94ed07da
Max Avramenko <71176948+darkforce1988@users.noreply.github.com>
2021-11-28 22:24:59 +0300

    solve 8.5 task

---
+++ Chapter 08/8.5.py
@@ -0,0 +1,11 @@
+def describe_city(city, country='Greece'):
+    print(f"{city.title()} is in {country}.")
+
+
+describe_city('Athens')
+print()
+
+describe_city(city='Chania')
+print()
+
+describe_city(city='Moscow')
