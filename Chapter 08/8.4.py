5406c33120f3238fec43bab68a503aa76bee58f2
Max Avramenko <71176948+darkforce1988@users.noreply.github.com>
2021-11-28 22:24:51 +0300

    solve 8.4 task

---
+++ Chapter 08/8.4.py
@@ -0,0 +1,9 @@
+def make_shirt(size='l', writing='I love Python'):
+    print(f"Shirt size: '{size.title()}'.")
+    print(f"Shirt writing: '{writing}'.")
+
+
+make_shirt()
+print()
+
+make_shirt(size='m', writing='The future is so bright, I gotta wear shades')
