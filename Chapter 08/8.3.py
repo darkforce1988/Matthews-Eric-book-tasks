c13ff991cc0806ecf93664a544d7dc383d9ab9fc
Max Avramenko <71176948+darkforce1988@users.noreply.github.com>
2021-11-28 22:24:40 +0300

    solve 8.3 task

---
+++ Chapter 08/8.3.py
@@ -0,0 +1,9 @@
+def make_shirt(size, writing):
+    print(f"Shirt size: '{size.title()}'.")
+    print(f"Shirt writing: '{writing}'.")
+
+
+make_shirt('xxxxxl', 'Max is Kachechek!')
+print()
+
+make_shirt(size='xxxs', writing='Jan is string bean')
