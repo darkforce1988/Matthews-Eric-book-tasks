c4f805a9e19ecd4b1eac83d5e1bb01e453badfc3
Max Avramenko <71176948+darkforce1988@users.noreply.github.com>
2021-11-29 17:58:12 +0300

    solve 8.10 task

---
+++ Chapter 08/8.10.py
@@ -0,0 +1,16 @@
+def send_messages(messages, sent_messages):
+    while messages:
+        message = messages.pop()
+        print(message)
+        sent_messages.append(message)
+
+
+messages = ['Hello, man!',
+            'Jan, you are a slowpoke!',
+            'There is no knowledge that is not power.', ]
+sent_messages = []
+
+send_messages(messages, sent_messages)
+
+print(messages)
+print(sent_messages)
