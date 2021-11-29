755e24b535a5090a4dd2de2240cec84398daf7f5
Max Avramenko <71176948+darkforce1988@users.noreply.github.com>
2021-11-29 17:58:05 +0300

    solve 8.9 task

---
+++ Chapter 08/8.9.py
@@ -0,0 +1,10 @@
+def show_messages(showing_message):
+    for message in showing_message:
+        print(message)
+
+
+messages = ['Hello, man!',
+            'Jan, you are a slowpoke!',
+            'There is no knowledge that is not power.', ]
+
+show_messages(messages)
