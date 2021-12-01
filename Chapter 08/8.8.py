efb6c2e728dedd5f46869d7c2aa714f211882524
Max Avramenko <71176948+darkforce1988@users.noreply.github.com>
2021-12-01 18:01:08 +0300

    delete unnecessary code

--- Chapter 08/8.8.py
+++ Chapter 08/8.8.py
@@ -6,18 +6,6 @@ def make_album(singer_name, album_name, number_of_tracks=None):
     return album_information
 
 
-album_dictionary = make_album('Scooter', 'Age of love')
-print(album_dictionary)
-
-album_dictionary = make_album('System of a down', 'Mesmerise')
-print(album_dictionary)
-
-album_dictionary = make_album('Bon Jovi', 'Have a nice day')
-print(album_dictionary)
-
-album_dictionary = make_album('Korn', 'Untouchables', 14)
-print(album_dictionary)
-
 while True:
     singer_input = input("Enter a singer name (type 'quit' to exit): ")
     if singer_input == 'quit':
