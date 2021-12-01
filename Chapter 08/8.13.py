0721212768667abc4e5d602e0b07cccd40681c3e
Max Avramenko <71176948+darkforce1988@users.noreply.github.com>
2021-12-01 18:04:46 +0300

    solve 8.13 task

---
+++ Chapter 08/8.13.py
@@ -0,0 +1,17 @@
+def build_profile(first, last, **user_info):
+    """Строит словарь с информацией о пользователе."""
+    user_info['first_name'] = first
+    user_info['last_name'] = last
+    return user_info
+
+
+user_profile = build_profile(
+    'albert',
+    'einstein',
+    hobby='gym',
+    nickname='F0rce',
+    favorite_game='Dota 2',
+    )
+
+full_name = user_profile['first_name'] + ' ' + user_profile['last_name']
+print(f"{full_name.title()}'s information:\n{user_profile}.")
