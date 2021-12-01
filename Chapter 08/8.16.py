f9169995362bf7466bca0a7f15404e1887a4d41f
Max Avramenko <71176948+darkforce1988@users.noreply.github.com>
2021-12-01 18:05:08 +0300

    solve 8.16 task

---
+++ Chapter 08/8.16.py
@@ -0,0 +1,18 @@
+import making_shirts
+making_shirts.make_shirt('s', 'Mortal kombat')
+print()
+
+from making_shirts import make_shirt
+make_shirt('l', 'Mortal kombat')
+print()
+
+from making_shirts import make_shirt as ms
+ms('xl', 'Mortal kombat')
+print()
+
+import making_shirts as make_s
+make_s.make_shirt('xxl', 'Mortal kombat')
+print()
+
+from making_shirts import *
+making_shirts.make_shirt('xxxxxxxxl', 'Mortal kombat')
