435da34e6f1e4aca068fc5bee980eba8d72ce830
Max Avramenko <71176948+darkforce1988@users.noreply.github.com>
2021-12-09 20:22:48 +0300

    solve 10.7 task

---
+++ Chapter 10/10.7.py
@@ -0,0 +1,15 @@
+while True:
+    try:
+        first_number = input('Enter the first number. Type "q" to quit: ')
+        if str(first_number) == 'q':
+            break
+        first_number = int(first_number)
+        second_number = input('Enter the second number to make addition: ')
+        if str(second_number) == 'q':
+            break
+        second_number = int(second_number)
+    except ValueError:
+        print("You have entered an incorrect number.")
+    else:
+        sum_numbers = first_number + second_number
+        print(f"The sum is: {sum_numbers}.")
