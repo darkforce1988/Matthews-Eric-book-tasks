18c894f2a14ad76771ddd0eec3092082c2893cf7
Max Avramenko <71176948+darkforce1988@users.noreply.github.com>
2021-12-12 21:58:52 +0300

    add 'file_name' variable

--- Chapter 10/10.2.py
+++ Chapter 10/10.2.py
@@ -1,13 +1,15 @@
-with open('learning_python.txt', encoding='UTF-8') as txt_file_object:
+file_name = 'learning_python.txt'
+
+with open(file_name, encoding='UTF-8') as txt_file_object:
     txt_file_contents = txt_file_object.read()
     print(txt_file_contents.replace('Python', 'JavaScript'))
 
-with open('learning_python.txt', encoding='UTF-8') as txt_file_object:
+with open(file_name, encoding='UTF-8') as txt_file_object:
     for line in txt_file_object:
         print(line.rstrip().replace('Python', 'JavaScript'))
 print()
 
-with open('learning_python.txt', encoding='UTF-8') as txt_file_object:
+with open(file_name, encoding='UTF-8') as txt_file_object:
     txt_file_contents_as_list_of_lines = txt_file_object.readlines()
 for line in txt_file_contents_as_list_of_lines:
     line = line.replace('Python', 'JavaScript')
