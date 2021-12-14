eb6dc4f80a6d415c2d6e7e17ddc7bb27d8b1b219
Max Avramenko <71176948+darkforce1988@users.noreply.github.com>
2021-12-14 17:43:19 +0300

    add defined function for words count and try-except block for open() function

--- Chapter 10/10.10.py
+++ Chapter 10/10.10.py
@@ -1,8 +1,16 @@
-filenames = ['Ancient Rome and Modern America.txt', 'Broken Barriers.txt']
-for filename in filenames:
-    with open(filename, encoding='utf-8') as f:
-        contents = f.read()
+def count_words(filename):
+    try:
+        with open(filename, encoding='utf-8') as f:
+            contents = f.read()
+    except FileNotFoundError:
+        print(f"File '{filename}' is not found.")
+    else:
         count = contents.lower().count('the')
         print(f"Number of 'the' in book '{filename}' is: {count}.")
         count = contents.lower().count('the ')
         print(f"Number of 'the ' in book '{filename}' is: {count}.")
+
+
+filenames = ['Ancient Rome and Modern America.txt', 'Broken Barriers.txt']
+for filename in filenames:
+    count_words(filename)
