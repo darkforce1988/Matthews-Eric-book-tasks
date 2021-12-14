c8ececc024a40b6b630ae176ff54c1502a9e2c8c
Max Avramenko <71176948+darkforce1988@users.noreply.github.com>
2021-12-14 17:36:00 +0300

    move open() function inside 'while' loop

--- Chapter 10/10.5.py
+++ Chapter 10/10.5.py
@@ -1,8 +1,9 @@
 file_name = 'reasons_you_like_programming.txt'
-with open(file_name, 'a', encoding='UTF-8') as file_object:
-    while True:
-        reason = input('Напишите, почему Вам нравится программировать? '
-                       'Для выхода введите "q": ')
-        if reason == 'q':
-            break
+
+while True:
+    reason = input('Напишите, почему Вам нравится программировать? '
+                   'Для выхода введите "q": ')
+    if reason == 'q':
+        break
+    with open(file_name, 'a', encoding='UTF-8') as file_object:
         file_object.write(reason + '\n')
