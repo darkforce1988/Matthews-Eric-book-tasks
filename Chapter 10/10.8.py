45f5d8c579feaf95096588e4f0a73947692219b3
Max Avramenko <71176948+darkforce1988@users.noreply.github.com>
2021-12-14 17:43:19 +0300

    add encoding for open() function and add block 'else' for 'try'

--- Chapter 10/10.8.py
+++ Chapter 10/10.8.py
@@ -2,8 +2,9 @@ filenames = ['cats.txt', 'dogs.txt', 'rabbits.txt']
 
 for filename in filenames:
     try:
-        with open(filename) as f:
+        with open(filename, encoding='UTF-8') as f:
             contents = f.read()
-            print(contents)
     except FileNotFoundError:
         print(f"File {filename} is not found.")
+    else:
+        print(contents)
