f156a0398213aeb417257ae615aa55c2ec9fcb2d
Max Avramenko <71176948+darkforce1988@users.noreply.github.com>
2022-01-25 00:43:42 +0300

    fix 9.14 task

--- Chapter 09/9.14.py
+++ Chapter 09/9.14.py
@@ -1,11 +1,13 @@
 from random import choice
 
-values = [1, 8, 22, 'a', 't', 3, 5, 2, 11, 17, 21, 10, 'x', 'y', 'z']
+values = (1, 8, 22, 'a', 't', 3, 5, 2, 11, 17, 21, 10, 'x', 'y', 'z')
+values_list = list(values)
 
 win_values_list = []
-for amount_of_win_values in range(4):
-    win_value = choice(values)
-    win_values_list.append(str(win_value))
+for i in range(4):
+    win_value = choice(values_list)
+    win_values_list.append(win_value)
+    values_list.remove(win_value)
 
 print(f"Any ticket matching these four numbers or letters wins a prize:"
       f"\n\t{win_values_list}")
