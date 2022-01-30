3142dbc65d8d5b205de5aaae9438206c63933551
Max Avramenko <71176948+darkforce1988@users.noreply.github.com>
2022-01-31 00:47:33 +0300

    use function for solving 9.14 task

--- Chapter 09/9.14.py
+++ Chapter 09/9.14.py
@@ -2,11 +2,16 @@ from random import choice
 
 values_pool = [1, 8, 22, 'a', 't', 3, 5, 2, 11, 17, 21, 10, 'x', 'y', 'z']
 
-win_values = []
-for i in range(4):
-    win_value = choice(values_pool)
-    win_values.append(win_value)
-    values_pool.remove(win_value)
 
-print(f"Any ticket matching these four numbers or letters wins a prize:"
-      f"\n\t{win_values}")
+def pick_combination(values_pool, values_combination_lenght=4):
+    combination_list = []
+    for i in range(values_combination_lenght):
+        value = choice(values_pool)
+        combination_list.append(value)
+        values_pool.remove(value)
+    return combination_list
+
+
+my_ticket = pick_combination(values_pool[:])
+print(f"Any ticket matching these four numbers or letters wins a prize: "
+      f"\n{my_ticket}")
