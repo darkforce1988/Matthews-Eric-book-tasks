fb3765d0a98e6a7f45ceffd2ffa71abc2b3864a2
Max Avramenko <71176948+darkforce1988@users.noreply.github.com>
2022-02-01 19:21:26 +0300

    improve UI: add text message in output

--- Chapter 10/10.1.py
+++ Chapter 10/10.1.py
@@ -1,15 +1,17 @@
 file_name = 'learning_python.txt'
 
-with open(file_name, encoding='UTF-8') as txt_file_object:
-    txt_file_contents = txt_file_object.read()
-    print(txt_file_contents)
+with open(file_name, encoding='UTF-8') as file_object:
+    file_contents = file_object.read()
+    print(f"The contents of a file '{file_name}':\n{file_contents}")
 
-with open(file_name, encoding='UTF-8') as txt_file_object:
-    for line in txt_file_object:
+with open(file_name, encoding='UTF-8') as file_object:
+    print(f"The contents of a file '{file_name}':")
+    for line in file_object:
         print(line.rstrip())
 print()
 
-with open(file_name, encoding='UTF-8') as txt_file_object:
-    txt_file_contents_as_list_of_lines = txt_file_object.readlines()
-for line in txt_file_contents_as_list_of_lines:
-    print(line.rstrip())
+with open(file_name, encoding='UTF-8') as file_object:
+    file_contents = file_object.readlines()
+    print(f"The contents of a file '{file_name}':")
+    for line in file_contents:
+        print(line.rstrip())
--- Chapter 10/10.2.py
+++ Chapter 10/10.2.py
@@ -1,16 +1,19 @@
 file_name = 'learning_python.txt'
 
-with open(file_name, encoding='UTF-8') as txt_file_object:
-    txt_file_contents = txt_file_object.read()
-    print(txt_file_contents.replace('Python', 'JavaScript'))
+with open(file_name, encoding='UTF-8') as file_object:
+    file_contents = file_object.read()
+    print(f"The contents of a modified file '{file_name}':\n"
+          f"{file_contents.replace('Python', 'JavaScript')}")
 
-with open(file_name, encoding='UTF-8') as txt_file_object:
-    for line in txt_file_object:
+with open(file_name, encoding='UTF-8') as file_object:
+    print(f"The contents of a modified file '{file_name}':")
+    for line in file_object:
         print(line.rstrip().replace('Python', 'JavaScript'))
 print()
 
-with open(file_name, encoding='UTF-8') as txt_file_object:
-    txt_file_contents_as_list_of_lines = txt_file_object.readlines()
-for line in txt_file_contents_as_list_of_lines:
+with open(file_name, encoding='UTF-8') as file_object:
+    file_contents = file_object.readlines()
+    print(f"The contents of a modified file '{file_name}':")
+for line in file_contents:
     line = line.replace('Python', 'JavaScript')
     print(line.rstrip())
