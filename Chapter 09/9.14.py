f2e3598d719526965b4e4b38e00570840a0f7336
Max Avramenko <71176948+darkforce1988@users.noreply.github.com>
2022-02-01 19:58:03 +0300

    use tuple of values instead of list, remove spelling mistake

--- Chapter 09/9.14.py
+++ Chapter 09/9.14.py
@@ -1,17 +1,17 @@
 from random import choice
 
-values_pool = [1, 8, 22, 'a', 't', 3, 5, 2, 11, 17, 21, 10, 'x', 'y', 'z']
+values_pool = (1, 8, 22, 'a', 't', 3, 5, 2, 11, 17, 21, 10, 'x', 'y', 'z')
 
 
-def pick_combination(values_pool, values_combination_lenght=4):
+def pick_combination(values_pool, combination_length=4):
     combination_list = []
-    for i in range(values_combination_lenght):
+    for i in range(combination_length):
         value = choice(values_pool)
         combination_list.append(value)
         values_pool.remove(value)
     return combination_list
 
 
-my_ticket = pick_combination(values_pool[:])
-print(f"Any ticket matching these four numbers or letters wins a prize: "
+my_ticket = pick_combination(list(values_pool))
+print(f"Any ticket matching these four numbers or letters wins a prize:"
       f"\n{my_ticket}")
