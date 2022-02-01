be556eb38c3baee766e9b997cc96fdf846ebdd66
Max Avramenko <71176948+darkforce1988@users.noreply.github.com>
2022-02-01 19:58:44 +0300

    remove spelling mistake

--- Chapter 09/9.14 v.2.py
+++ Chapter 09/9.14 v.2.py
@@ -3,9 +3,9 @@ from random import choice
 values_pool = (1, 8, 22, 'a', 't', 3, 5, 2, 11, 17, 21, 10, 'x', 'y', 'z')
 
 
-def pick_combination(values_pool, values_combination_lenght=4):
+def pick_combination(values_pool, combination_length=4):
     combination_list = []
-    while len(combination_list) < values_combination_lenght:
+    while len(combination_list) < combination_length:
         value = choice(values_pool)
         if value not in combination_list:
             combination_list.append(value)
