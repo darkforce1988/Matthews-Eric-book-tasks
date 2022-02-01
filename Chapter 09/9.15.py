553fa9ca67db6ec63625ec3ba7f513dd1c47d112
Max Avramenko <71176948+darkforce1988@users.noreply.github.com>
2022-02-01 20:23:30 +0300

    improve 9.15 task

--- Chapter 09/9.15.py
+++ Chapter 09/9.15.py
@@ -1,26 +1,24 @@
 from random import choice
 
-COMBINATION_LENGHT = 4
 values_pool = (1, 8, 22, 'a', 't', 3, 5, 2, 11, 17, 21, 10, 'x', 'y', 'z')
 
 
-def pick_combination(values=values_pool,
-                     values_combination_lenght=COMBINATION_LENGHT):
+def pick_combination(values=values_pool, combination_length=4):
     combination_list = []
-    while len(combination_list) < values_combination_lenght:
+    while len(combination_list) < combination_length:
         value = choice(values)
         if value not in combination_list:
             combination_list.append(value)
     return combination_list
 
 
-win_ticket = pick_combination()
+win_combination = pick_combination()
 
 my_ticket = pick_combination()
-tries = 0
-while my_ticket != win_ticket:
+tries = 1
+while my_ticket != win_combination:
     my_ticket = pick_combination()
     tries += 1
 
-print(f"Winning ticket is: {win_ticket}\n"
+print(f"Winning ticket is: {my_ticket}\n"
       f"Amount of tries to win the lottery: {tries}")
